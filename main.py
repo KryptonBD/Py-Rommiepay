0e38517a6459f2482c83eb273321571eadbbad80
KryptonBD <foysol.sahi@gmail.com>
2024-08-23 12:33:28 +0100

    Payment

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+from typing import Self
+
 class Bill:
   '''
   Contains the data for a bill
   amount and period of the bill
   '''
 
-  def __init__(self,amount, period):
+  def __init__(self, amount: int, period: str):
     self.amount = amount
     self.period = period
 
@@ -14,12 +16,14 @@ class FlatMate:
   Creates a flatmate person class and pays a share of the bill
   '''
 
-  def __init__(self, name, days_in_house):
+  def __init__(self, name: str, days_in_house: int):
     self.name = name
     self.days_in_house = days_in_house
 
-  def pays(self, bill):
-    return bill.amount / 2
+  def pays(self, bill: Bill, other_flat_mate: Self) -> float | int:
+    total_days = self.days_in_house + other_flat_mate.days_in_house
+
+    return bill.amount * (self.days_in_house / total_days)
 
 class PdfReport:
   '''
@@ -29,7 +33,7 @@ class PdfReport:
   def __init__(self, filename):
     self.filename = filename
 
-  def generate(self, f1, fm2, bill):
+  def generate(self, fm1: FlatMate, fm2: FlatMate, bill: Bill):
     pass
 
 
@@ -38,4 +42,4 @@ bill = Bill(amount=480, period='08/24')
 bruce = FlatMate(name='Bruce', days_in_house=21)
 oliver = FlatMate(name='Oliver', days_in_house=26)
 
-print(bruce.pays(bill))
+print(bruce.pays(bill, oliver))
